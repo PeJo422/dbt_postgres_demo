508f81862e9679348527704652179594eeb7ac6e
Peter Johannessen <peter.johannessen@columbusglobal.com>
2025-04-02 12:13:27 +0000

    total

--- models/total_sales_by_customer.sql
+++ models/total_sales_by_customer.sql
@@ -1,8 +1,9 @@
 SELECT 
-usr.name
-,sum(t.amount) as Amount
-
+usr.name as "Customer Name"
+,COALESCE(date_part('year',t.purchase_date),1999) as "Year"
+,sum(t.amount) as "Amount"
+,Count(*) as "Transaction count"
 FROM {{ref("transactions")}} t
 INNER JOIN {{ref("users")}} usr on t.user_id = usr.id
-GROUP BY usr.name
-
+GROUP BY usr.name ,date_part('year',t.purchase_date)
+order by "Customer Name" Asc, "Year"  desc
