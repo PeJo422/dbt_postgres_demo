a4e9db801b06d57d6f5dacef1a42b6d021b305e6
Peter Johannessen <peter.johannessen@columbusglobal.com>
2025-04-14 13:59:12 +0000

    1

--- models/total_sales_by_customer.sql
+++ models/total_sales_by_customer.sql
@@ -10,18 +10,3 @@ FROM {{ref("fact_transactions")}} t
 INNER JOIN {{ref("dim_users")}} usr ON t.user_id = usr.id
 GROUP BY ROLLUP (usr.name, DATE_PART('year', t.purchase_date))
 ORDER BY "Customer Name" ASC, "Year" DESC
-
-
-/*
-SELECT 
-usr.name as "Customer Name"
-,COALESCE(date_part('year',t.purchase_date),1999) as "Year"
-,sum(t.amount) as "Amount"
-,Count(*) as "Transaction count"
-
-FROM {{ref("fact_transactions")}} t
-INNER JOIN {{ref("dim_users")}} usr on t.user_id = usr.id
-
-GROUP BY usr.name ,date_part('year',t.purchase_date)
-order by "Customer Name" Asc, "Year"  desc
-*/
