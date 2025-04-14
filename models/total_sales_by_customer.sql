940ef06f7fd9579f808982e9f7b60d46520d0cac
Peter Johannessen <peter.johannessen@columbusglobal.com>
2025-04-14 14:42:56 +0000

    rts

--- models/total_sales_by_customer.sql
+++ models/total_sales_by_customer.sql
@@ -1,12 +1,12 @@
-SELECT 
-  usr.name AS "Customer Name",
-  COALESCE(DATE_PART('year', t.purchase_date), 1999) AS "Year",
-  SUM(t.amount) AS "Total Amount",
-  COUNT(*) AS "Transaction Count",
-  MAX(t.amount) AS "Max Amount",
-  MIN(t.amount) AS "Min Amount",
-  AVG(t.amount) AS "Avg Amount"
-FROM {{ref("fact_transactions")}} t
-INNER JOIN {{ref("dim_users")}} usr ON t.user_id = usr.id
-GROUP BY ROLLUP (usr.name, DATE_PART('year', t.purchase_date))
-ORDER BY "Customer Name" ASC, "Year" DESC
+select
+    usr.name as "Customer Name",
+    coalesce(date_part('year', t.purchase_date), 1999) as "Year",
+    sum(t.amount) as "Total Amount",
+    count(*) as "Transaction Count",
+    max(t.amount) as "Max Amount",
+    min(t.amount) as "Min Amount",
+    avg(t.amount) as "Avg Amount"
+from {{ ref("fact_transactions") }} t
+inner join {{ ref("dim_users") }} usr on t.user_id = usr.id
+group by rollup (usr.name, date_part('year', t.purchase_date))
+order by "Customer Name" asc, "Year" desc
